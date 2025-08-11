e8ae9217882789599c4f01bd22dc5459269cf3da
JANGSAMMY <toal7822@gmail.com>
2025-08-11 12:07:30 +0900

    새미추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Test1 from './components/Test1';
 import Test2 from './components/Test2';
 import Test3 from './components/Test3';
+import Test4 from './components/Test4';
 
 const App = () => {
     return (
         <div>
+            <Test4 />
             <Test3 />
             <Test2 />
             <Test1 />
---
+++ src/components/Test4.jsx
@@ -0,0 +1,5 @@
+const Test4 = () => {
+    return <div>새미</div>;
+};
+
+export default Test4;
