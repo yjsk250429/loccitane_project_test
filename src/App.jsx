257d2c7a3c4817e453d6808e313867621c630b2d
gkstmftt <yanglily328@gmail.com>
2025-08-11 11:43:11 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import Test2 from "./components/Test2";
+import Test1 from './components/Test1';
+import Test2 from './components/Test2';
 
 const App = () => {
-    return <div><Test2/></div>;
+    return (
+        <div>
+            <Test2 />
+            <Test1 />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Test1.jsx
@@ -0,0 +1,10 @@
+
+const Test1 = () => {
+    return (
+        <div>
+            양한슬
+        </div>
+    );
+};
+
+export default Test1;
