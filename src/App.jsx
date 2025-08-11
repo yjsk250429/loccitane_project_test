1ea82e7a0a6f3590a6b3ab9841c223c32dc7ac64
SohnSangbum <foot0707@gmail.com>
2025-08-11 11:54:37 +0900

    상범추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Test1 from './components/Test1';
 import Test2 from './components/Test2';
+import Test3 from './components/Test3';
 
 const App = () => {
     return (
         <div>
+            <Test3 />
             <Test2 />
             <Test1 />
         </div>
---
+++ src/components/Test3.jsx
@@ -0,0 +1,9 @@
+const Test3 = () => {
+    return (
+        <div>
+            <h2>상범</h2>
+        </div>
+    );
+};
+
+export default Test3;
