294e4e2432b89ca9e178b2aba81ff0c8da702bbc
yjsk250429 <yejin1046@gmail.com>
2025-08-11 11:26:06 +0900

    예진2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Test2 from "./components/Test2";
+
 const App = () => {
-    return <div>test</div>;
+    return <div><Test2/></div>;
 };
 
 export default App;
---
+++ src/components/Test2.jsx
@@ -0,0 +1,10 @@
+
+const Test2 = () => {
+    return (
+        <div>
+            정예진
+        </div>
+    );
+};
+
+export default Test2;
